8afc5fc2c4f876d85c01504f8789772d7fa585ca
Taha Mostafa <20231700229@cis.asu.edu.eg>
2024-11-25 00:15:13 +0200

    The entire Pet Project Folder
    IntellIJ Pet Project Folder

---
+++ Pet_Shelter/src/main/java/com/example/pet_shelter/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.pet_shelter;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
