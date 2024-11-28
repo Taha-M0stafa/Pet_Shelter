34236ff708765c73058d9ebaa1908ea9eeadecc6
asmaa <20231700208@cis.asu.edu.eg>
2024-11-29 01:42:52 +0200

    lilac is here

--- Pet_Shelter/src/main/java/com/example/pet_shelter/HelloApplication.java
+++ Pet_Shelter/src/main/java/com/example/pet_shelter/HelloApplication.java
@@ -29,7 +29,7 @@ public class HelloApplication extends Application {
         changeStage.getScene().setRoot(changeRoot);
         changeStage.show();
     }
-
+    //hi Thanoid
     public static void main(String[] args) {
 
         launch();
